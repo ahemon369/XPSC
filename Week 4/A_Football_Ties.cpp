aa7e465214db3564b706f386910fce5f8239bdd1
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-04-19 23:00:48 +0600

    A_Football_Ties.cpp

---
+++ Week 4/A_Football_Ties.cpp
@@ -0,0 +1,14 @@
+/*     Author: Md Amran Hossin Emon
+       Created: 2025-04-19 11:00:00*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin>>t;
+    while(t--)
+    {
+        int x,y; cin>>x>>y;
+        cout<<x%3<<'\n';
+    }
+    return 0;
+}
