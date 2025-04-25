c255d5d900991e5641a7384c6d30316e23f32930
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-04-25 11:20:58 +0600

    N_Powering_the_Hero_hard_version.cpp

--- Week 3 Topicwise STL - 2/N_Powering_the_Hero_hard_version.cpp
+++ Week 3 Topicwise STL - 2/N_Powering_the_Hero_hard_version.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*     Author: Md Amran Hossin Emon
+       Created: 2025-04-25 10:00:00*/
 int main()
 {
     ios::sync_with_stdio(false);
@@ -21,6 +23,36 @@ int main()
 				pq.pop();			
             }
 		}
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
+
+/* 2nd away slove */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/*     Author: Md Amran Hossin Emon
+       Created: 2025-04-25 10:00:00*/
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin>>t;
+
+    while (t--) {
+        int n; cin>>n;
+        priority_queue<int> pq;  
+        long long ans=0;
+        for (int i=0;i<n;i++) {
+            int x; cin>>x;
+            if (x) pq.push(x);
+            else if (!pq.empty()) {
+                ans+=pq.top();
+                pq.pop();
+            }
+        }
         cout<<ans<<'\n';
     }
     return 0;
