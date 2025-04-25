97e8a9f8e6497b09e56f6c2aa7c964488b14a1f8
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-04-25 23:49:46 +0600

    J_Numbers_on_Whiteboard.cpp

--- Week 3 Topicwise STL - 2/J_Numbers_on_Whiteboard.cpp
+++ Week 3 Topicwise STL - 2/J_Numbers_on_Whiteboard.cpp
@@ -3,28 +3,19 @@ using namespace std;
 
 int main()
 {
+    /*     Author: Md Amran Hossin Emon
+           Created: 2025-04-24 07:30:00*/
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int t,n; cin>>t;
-    while(t--){
-        priority_queue<int>pq;
-        cin>>n;
-        for(int i=n; i>=1; i--){
-            pq.push(i);
-        }
-        int a,b,c;
-        cout<<2<<endl;
-        while(pq.size() > 1){
-            a = pq.top(); pq.pop();
-            b = pq.top();
-            pq.pop();
-            cout<<a<<" "<<b<<endl;
-            c = a + b;
-            if(c & 1)c++;
-            c /= 2;
-            pq.push(c);
-        }
-    }
-    return 0;
+	int t; cin>>t;
+	while(t--){
+		int n; cin>>n;
+		cout<<"2\n";
+		cout<<n<<" "<<n-1<<'\n';
+		while(n>=3){   
+			cout<<n<<" "<<n-2<<'\n';
+			n--;
+		}
+	}
+	return 0;
 }
