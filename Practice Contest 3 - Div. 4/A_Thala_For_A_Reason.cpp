4224af5de20ced332eac13d8f5c2fcbc63e9c4fa
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-04-30 23:14:55 +0600

    Add files via upload

---
+++ Practice Contest 3 - Div. 4/A_Thala_For_A_Reason.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*     Author: Md Amran Hossin Emon
+       Created: 2025-04-29 09:00:00*/
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin>>n; 
+
+    if (n==7) cout<<"THALA\n";
+    else cout<<"SADGE\n";
+    return 0;
+}
