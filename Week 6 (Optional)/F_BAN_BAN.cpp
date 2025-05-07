9331acfa287e6a756dc42724679eb44ae631295d
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-05-07 23:15:12 +0600

    F_BAN_BAN.cpp

--- Week 6 (Optional)/F_BAN_BAN.cpp
+++ Week 6 (Optional)/F_BAN_BAN.cpp
@@ -1,2 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
 
-
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        for(int i=0;i<n;i++){
+            if(i%2) arr[i]=i
+        }
+    }
+    return 0;
+}
