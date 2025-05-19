a148a774fd4255e39ce764ba5499b2875ba15d04
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-05-19 13:42:43 +0600

    C_Rock_and_Lever.cpp

--- Week 8 Bit Manipulation - 2/C_Rock_and_Lever.cpp
+++ Week 8 Bit Manipulation - 2/C_Rock_and_Lever.cpp
@@ -6,22 +6,17 @@ const int maxN = 30;
 int main() {
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
-   int t;
-   cin >> t;
+   int t; cin>>t;
    while (t--) {
-      int n;
-      cin >> n;
+      int n; cin>>n;
       vector<int> cnt(maxN);
-      for (int i = 1;i <= n;i++) {
-         int x;
-         cin >> x;
+      for (int i=1;i<=n;i++) {
+         int x; cin>>x;
          cnt[__lg(x)]++;
       }
-      long long ans = 0;
-      for (int i = 0;i < maxN;i++) {
-         ans += (1LL * cnt[i] * (cnt[i] - 1)) / 2;
-      }
-      cout << ans << '\n';
+      long long ans=0;
+      for (int i=0;i<maxN;i++)  ans+=(1LL*cnt[i]*(cnt[i]-1))/2;
+      cout<<ans<<'\n';
    }
    return 0;
 }
