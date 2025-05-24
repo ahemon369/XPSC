f476e0b9f7f51cf5a552a72d59dd0ce692608046
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-05-24 22:22:09 +0600

    G_Fedor_and_New_Game.cpp

--- Week 8 Bit Manipulation - 2/G_Fedor_and_New_Game.cpp
+++ Week 8 Bit Manipulation - 2/G_Fedor_and_New_Game.cpp
@@ -1,23 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
-    
+    cin.tie(nullptr);
     int n,m,k; cin>>n>>m>>k;
-    int a[n];
+    vector<int> a(m+1);
+    for (int i=0;i<=m;i++) cin>>a[i];
 
-    for (int i=0;i<m+1;i++) cin>>a[i];
-    int ans= 0;
-    for (int i = 0; i < m; i++)
-    {
-        int cnt=0;
-        for (int j=0;j<n;j++)
-            if (((a[i]>>j)&1) != ((a[m]>>j)&1)) cnt++;
-        if(cnt<=k) ans++;
+    int ans=0;
+    for (int i=0;i<m;i++) {
+        if (__builtin_popcount(a[i]^a[m])<=k) ans++;
     }
     cout<<ans<<'\n';
-    return 0;
 }
