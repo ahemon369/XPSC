af81d44d134edc97ffe18fd86b724315aea4df45
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-05-25 21:16:49 +0600

    A_k_th_divisor.cpp

---
+++ Week 9 Math And Number Theory - 1/A_k_th_divisor.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   ll n,k; cin>>n>>k;
+   vector<ll> v;
+   for (ll i=1;i*i<=n;i++) {
+      if (n%i==0) {
+         v.push_back(i);
+         if ((i!=n/i)) v.push_back(n/i);
+      }
+   }
+   sort(v.begin(), v.end());
+   if (v.size()<k) cout<<-1<<'\n';
+   else cout<<v[k-1]<<'\n';
+   return 0;
+}
