7e235788d032f60ed5b210bf8ad47596e2d5ccff
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-05-27 12:26:02 +0600

    G_Relatively_Prime_Pairs.cpp

--- Week 9 Math And Number Theory - 1/G_Relatively_Prime_Pairs.cpp
+++ Week 9 Math And Number Theory - 1/G_Relatively_Prime_Pairs.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve() {
+    ll l,r;cin>>l>>r;
+    cout<<"YES\n";
+    for (ll i=l;i<=r;i+=2) cout<<i<<" "<<i+1<<'\n';
+}
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve(); 
+    return 0;
+}
