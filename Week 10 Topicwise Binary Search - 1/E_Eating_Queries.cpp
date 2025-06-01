41156701c1c554e920b23ab6e54f8457891d0971
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-06-01 20:16:28 +0600

    E_Eating_Queries.cpp

---
+++ Week 10 Topicwise Binary Search - 1/E_Eating_Queries.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin>>t;
+    while (t--) {
+        int n,q; cin>>n>>q;
+        vector<int> a(n), pre(n+1);
+        for (int i=0;i<n;i++) cin>>a[i];
+        sort(a.rbegin(), a.rend());
+        for (int i=1;i<=n;i++) pre[i]=pre[i-1]+a[i-1];
+        for (int i=0;i<q;i++) {
+            int k,ans=-1; cin>>k;
+            auto it=lower_bound(pre.begin(), pre.end(),k);
+            if (it!=pre.end()) ans=it-pre.begin();
+            cout<<ans<<'\n';
+        }
+    }
+    return 0;
+}
