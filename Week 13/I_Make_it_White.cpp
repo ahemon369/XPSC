703fb864cc9f0fa19438be66705d204f15ef1d51
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-06-25 18:34:39 +0600

    I_Make_it_White.cpp

--- Week 13/I_Make_it_White.cpp
+++ Week 13/I_Make_it_White.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int t; cin>>t;
+   for(int i=0;i<t;i++){
+      int n; cin>>n;
+      string s; cin>>s;
+      int firstB=-1,lastB=-1;
+      firstB=s.find('B');
+      lastB=s.rfind('B');
+      int ans=lastB - firstB + 1;
+      cout<<ans<<'\n';
+   }
+   return 0;
+}
