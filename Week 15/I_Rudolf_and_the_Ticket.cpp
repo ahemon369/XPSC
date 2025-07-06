9638372d59872d3ad3a2bf495cbaf116fa755a90
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-07-07 00:36:27 +0600

    I_Rudolf_and_the_Ticket.cpp

--- Week 15/I_Rudolf_and_the_Ticket.cpp
+++ Week 15/I_Rudolf_and_the_Ticket.cpp
@@ -12,9 +12,7 @@ int main()
         for(int i=0;i<n;i++) cin>>a[i];
         for(int i=0;i<m;i++) cin>>b[i];
         for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(a[i]+b[j]<=k) ans++;
-            }
+            for(int j=0;j<m;j++) {if(a[i]+b[j]<=k) ans++;}
         }
         cout<<ans<<'\n';
     }
